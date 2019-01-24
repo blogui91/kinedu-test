bd76fcb001378b8ffa004c685051580f5ac6bda3
Cesar Santana <casc.santana@gmail.com>
2019-01-24 01:14:05 -0600

    solve issue with env variables injector to make it work with netlify

--- config/env.js
+++ config/env.js
@@ -1,12 +1,33 @@
-const DotEnv = require('dotenv')
+const { config, parse } = require('dotenv')
 
+const defaultOptions = {
+    encoding: 'utf8',
+    examplePath: process.cwd()
+}
 module.exports = {
-  parse () {
-    const parsedEnv = DotEnv.config().parsed
-    const env = {}
-    for (let key in parsedEnv) {
-      env[key] = JSON.stringify(parsedEnv[key])
+    parse: function () {
+
+        const configOptions = { ...defaultOptions }
+        const parsedEnv = config(configOptions)
+        const appEnv = {}
+        for (const key in parsedEnv) {
+            if (parsedEnv.hasOwnProperty(key)) {
+                appEnv[key] = JSON.stringify(parsedEnv[key])
+            }
+        }
+
+        const fs = require('fs')
+        const path = require('path')
+        const encoding = configOptions.encoding
+        const baseEnvPath = path.resolve(configOptions.examplePath, '.env.example')
+        const baseEnvBuffer = fs.readFileSync(baseEnvPath, { encoding })
+        const baseEnv = parse(baseEnvBuffer)
+        for (const key in baseEnv) {
+            if (baseEnv.hasOwnProperty(key)) {
+                appEnv[key] = JSON.stringify(process.env[key])
+            }
+        }
+
+        return appEnv
     }
-    return env
-  }
 }

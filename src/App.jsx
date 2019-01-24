c318855123b0194e39e65fd1599970fd1efe1ea3
Cesar Santana <casc.santana@gmail.com>
2019-01-23 21:21:51 -0600

    Create skeleton view

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import './app.scss'
+import { Rounded } from 'src/components/Buttons'
+
+import Navbar from 'components/Navbar'
+import Icon from 'src/components/Icon/index.jsx'
 
 function App (props) {
+
   return (
-    <p> Hello world! </p>
+    <>
+      <Navbar fixed={true}/>
+      <div style={{transform: 'translateY(-50%)', position: 'fixed', top: '50%', 'left': '10px'}}>
+        <Rounded color="#75B753">
+          <Icon name="arrow_back" color="#fff"/>
+        </Rounded>
+      </div>s
+      <div className="container">
+      </div>
+      <div style={{transform: 'translateY(-50%)', position: 'fixed', top: '50%', 'right': '10px'}}>
+        <Rounded color="#75B753">
+          <Icon name="arrow_forward" color="#fff"/>
+        </Rounded>
+      </div>
+    </>
   )
 }
 

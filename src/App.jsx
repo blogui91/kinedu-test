f7d08cda0362eb63831b97922d6129fbc6bb5b7c
Cesar Santana <casc.santana@gmail.com>
2019-01-23 20:49:42 -0600

    Create react base code with styles

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './app.scss'
+
+function App (props) {
+  return (
+    <p> Hello world! </p>
+  )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from 'src/App.jsx';
+
+render(<App title="Kinedu App" />, document.getElementById('root'))

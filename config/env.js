3a37db2ea1266a73db37f47eca78f8ea2b8aab16
Cesar Santana <casc.santana@gmail.com>
2019-01-25 11:13:30 -0600

    ignore eslint rules in config/env file

--- config/env.js
+++ config/env.js
@@ -1,33 +1,31 @@
-const { config, parse } = require('dotenv')
+const { config, parse } = require('dotenv');
+const fs = require('fs');
+const path = require('path');
 
 const defaultOptions = {
-    encoding: 'utf8',
-    examplePath: process.cwd()
-}
+  encoding: 'utf8',
+  examplePath: process.cwd(),
+};
+
 module.exports = {
-    parse: function () {
-
-        const configOptions = { ...defaultOptions }
-        const parsedEnv = config(configOptions)
-        const appEnv = {}
-        for (const key in parsedEnv) {
-            if (parsedEnv.hasOwnProperty(key)) {
-                appEnv[key] = JSON.stringify(parsedEnv[key])
-            }
-        }
-
-        const fs = require('fs')
-        const path = require('path')
-        const encoding = configOptions.encoding
-        const baseEnvPath = path.resolve(configOptions.examplePath, '.env.example')
-        const baseEnvBuffer = fs.readFileSync(baseEnvPath, { encoding })
-        const baseEnv = parse(baseEnvBuffer)
-        for (const key in baseEnv) {
-            if (baseEnv.hasOwnProperty(key)) {
-                appEnv[key] = JSON.stringify(process.env[key])
-            }
-        }
-
-        return appEnv
+  parse() {
+    const configOptions = { ...defaultOptions };
+    const parsedEnv = config(configOptions);
+    const appEnv = {};
+    for (const key in parsedEnv) {
+      if (parsedEnv.hasOwnProperty(key)) {
+        appEnv[key] = JSON.stringify(parsedEnv[key]);
+      }
     }
+    const encoding = configOptions.encoding;
+    const baseEnvPath = path.resolve(configOptions.examplePath, '.env.example');
+    const baseEnvBuffer = fs.readFileSync(baseEnvPath, { encoding });
+    const baseEnv = parse(baseEnvBuffer);
+    for(const key in baseEnv) {
+      if (baseEnv.hasOwnProperty(key)) {
+        appEnv[key] = JSON.stringify(process.env[key]);
+      }
+    }
+    return appEnv;
+  }
 }
